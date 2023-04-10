7541091706aabda5950c34a9c81e60d9421b1ace
Calamity Crusher <calamity@Calamitys-MacBook-Pro.local>
2023-04-10 18:48:02 +0300

    added pickers of methods, added font garamond

--- FinansialState/View/MainPage.xaml.cs
+++ FinansialState/View/MainPage.xaml.cs
@@ -2,24 +2,61 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    string PickedMethodFinState = "";
+    string PickedMethodEfficiency = "";
+    string PickedMethodResults = "";
 
-	public MainPage()
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    void PickerSelectedIndexChangedFinState(object sender, EventArgs e)
+    {
+        PickedMethodFinState = $"{MethodPickerFinState.SelectedItem}";
+    }
+
+    void PickerSelectedIndexChangedEfficiency(object sender, EventArgs e)
+    {
+        PickedMethodEfficiency = $"{MethodPickerEfficiency.SelectedItem}";
+    }
+
+    void PickerSelectedIndexChangedResults(object sender, EventArgs e)
+    {
+        PickedMethodResults = $"{MethodPickerResults.SelectedItem}";
+    }
+
+    /* private void importExcel_Click(object sender, EventArgs e)
 	{
-		InitializeComponent();
-	}
+        //Create an instance of ExcelEngine.    
+        using (ExcelEngine excelEngine = new ExcelEngine())
+        {
+            IApplication application = excelEngine.Excel;
+            application.DefaultVersion = ExcelVersion.Xlsx;
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+            Assembly executingAssembly = typeof(App).GetTypeInfo().Assembly;
+            Stream inputStream = executingAssembly.GetManifestResourceStream("MAUISample.Sample.xlsx");
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+            //Create a workbook with a worksheet
+            IWorkbook workbook = application.Workbooks.Open(inputStream);
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+            //Access first worksheet from the workbook instance.
+            IWorksheet worksheet = workbook.Worksheets[0];
+
+            //Set Text in cell A3.
+            worksheet.Range["A3"].Text = "Hello World";
+
+            //Access a cell value from Excel
+            var value = worksheet.Range["A1"].Value;
+
+            MemoryStream ms = new MemoryStream();
+            workbook.SaveAs(ms);
+            ms.Position = 0;
+
+            //Saves the memory stream as a file.
+            DependencyService.Get<ISave>().SaveAndView("Invoice.xlsx", "application/excel", ms);
+        }
+    }   */
 }
 
 
