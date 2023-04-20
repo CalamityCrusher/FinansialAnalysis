e92f90e60bdf23c489184d90d62ce652fff64987
Calamity Crusher <calamity@Calamitys-MacBook-Pro.local>
2023-04-20 13:07:22 +0300

    program built for maccatalyst, ios16.4, android13

---
+++ FinansialState/obj/Debug/net7.0-android/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,12 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("FinansialState.MainApplication, FinansialState, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc643a5e505522a68332.MainApplication.class, crc643a5e505522a68332.MainApplication.__md_methods);
+		mono.android.Runtime.register ("Microsoft.Maui.MauiApplication, Microsoft.Maui, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc6488302ad6e9e4df1a.MauiApplication.class, crc6488302ad6e9e4df1a.MauiApplication.__md_methods);
+		
+	}
+}
